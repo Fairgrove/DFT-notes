fa1bb8ac4edb00bb623abf1d970ee1c85f53cad2
OEM Configuration (temporary user) <fagerlundfr@gmail.com>
2021-09-08 12:14:53 +0200

    started convolution algorithm

--- scripts/signal.py
+++ scripts/signal.py
@@ -1,14 +1,16 @@
 import math
 from random import randint
 
-class Signal:
+class System:
     signals = []
-    signal = []
+    inputSignal = []
     frequencies = []
+    outputSignal = []
 
-    def __init__(self, fs, t):
+    def __init__(self, fs, t, h):
         self.fs = fs #sample rate
         self.t = t #sample time
+        self.h = h #impulse response
 
 
     def randSignal(self, min, max):
@@ -48,6 +50,42 @@ class Signal:
             self.signals[-1].append(y)
 
             if len(self.signals) > 1:
-                self.signal[n-1] += y
+                self.inputSignal[n-1] += y
             else:
-                self.signal.append(y)
+                self.inputSignal.append(y)
+
+        #[sys.inputSignal,range(len(sys.inputSignal))]
+        self.calcOutputSignal()
+
+    def calcOutputSignal(self):
+        #convolve inputSignal with impulse response
+        #this is done by mirroring the impulse response about the origin
+        #each sample in the inputSignal is added
+        #    with every sample of the impulse response
+
+        hMirror = [[],[]]
+        for n in range(len(self.h[0])):
+            hMirror[0].append(self.h[0][-1-n])
+            hMirror[1].append(self.h[1][-1-n])
+
+        #convolution (x[n] * h[n])
+
+def convolve(x1, x2):
+    result = [[],[]]
+
+    for nx1 in x1[0]:
+        for nx2 in x2[0]:
+            n = nx1 + nx2
+
+
+                
+
+
+h = [[0,1],[2,1]]
+sys = System(10000, 1, h)
+sys.newSignal(10,1)
+
+a = [sys.inputSignal,range(len(sys.inputSignal))]
+print(len(a[0]), '  ', len(a[1]))
+print(a[0])
+print(a[1][0])
